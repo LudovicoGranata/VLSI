b31147c36b2ffcee7cf498a4ff9adc22fb53822c
Simone Persiani <iosonopersia@gmail.com>
2021-12-02 23:08:14 +0100

    Fix plot function

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ def get_cmap(n, name = 'hsv'):
   return plt.cm.get_cmap(name, n)
 
 def plot(dir, instance, out):
+  plt.figure(figsize=(8, 8))
   ax = plt.gca()
   cmap = None
 
@@ -13,18 +14,18 @@ def plot(dir, instance, out):
       line = line.replace("\n","")
       if (i==0):
         width, height = line.split()
-        canvas = plt.Rectangle ( (0,0), width=int(width), height=int(height), facecolor= "none", edgecolor="black")
+        plt.xticks([n for n in range(int(width) + 1)])
+        plt.yticks([n for n in range(int(height) + 1)])
+        canvas = plt.Rectangle((0,0), width=int(width), height=int(height), facecolor="none", edgecolor="black")
         ax.add_patch(canvas)
-      if (i==1):
+      elif (i==1):
         n_circuits = int(line)
         cmap = get_cmap(n_circuits)
-
-      if(i>1):
+      else:
         c_width, c_height, c_bottom_left_x, c_bottom_left_y =  line.split()
-        circuit = plt.Rectangle ( (int(c_bottom_left_x),int(c_bottom_left_y)), width=int(c_width), height=int(c_height), facecolor= cmap(i-2), edgecolor="black")
+        circuit = plt.Rectangle((int(c_bottom_left_x), int(c_bottom_left_y)), width=int(c_width), height=int(c_height), facecolor=cmap(i-2), edgecolor="black")
         ax.add_patch(circuit)
       i += 1
 
   plt.axis("scaled")
   plt.savefig(out+"/"+instance.split(".")[0]+"_out.png")
-  #plt.show()
