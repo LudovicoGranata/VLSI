5aa844104cd0efd9f138b83aac4b54b7396013c9
LudovicoGranata <Ludogranata@gmail.com>
2022-04-04 15:13:16 +0200

    ad custom path to solve_cp

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ import minizinc as mz
 from sat_utils import *
 import plot
 
-def solve_cp():
+
+def solve_cp(model_path):
     Total_instances = 40
     Solved_instances = 0
     Average_time = 0
     gecode = mz.Solver.lookup("gecode")
-    for i in range(1, 41):
+
+    for i in range(1, Total_instances + 1):
         print(f">>> Trying to solve instance #{i}...")
-        VLSI_model = mz.Model("./VLSI_model.mzn")
-        VLSI_model.add_file("./instances_cp/ins-"+str(i)+".dzn")
+        VLSI_model = mz.Model(model_path)
+        VLSI_model.add_file("./CP/ins/ins-"+str(i)+".dzn")
         instance = mz.Instance(gecode, VLSI_model)
         result = instance.solve(timeout=datetime.timedelta(seconds=30))
         #print(result)
@@ -24,13 +26,16 @@ def solve_cp():
             print("solved instance " + str(i) + " in = " + str(round(solving_time, 3)))
             Solved_instances+=1
             Average_time += solving_time
-            with open('./out_cp/ins-' + str(i) + '.txt', 'w') as writefile:
+            with open('./CP/out/ins-' + str(i) + '.txt', 'w') as writefile:
                 writefile.write(str(result))
-            plot.plot("./out_cp","ins-"+str(i)+".txt","./out_cp/images")
+            plot.plot("./CP/out","ins-"+str(i)+".txt","./CP/out/images")
         else:
             print("instance: "+str(i)+" not solved in the timelimit")
-    Average_time = Average_time/Solved_instances
-    print ("the average solving time of the solved instances_cp is "+str(Average_time)+ " seconds and the solved instances_cp are "+str(Solved_instances)+"/40" )
+    if Solved_instances!=0:
+        Average_time = Average_time/Solved_instances
+        print ("the average solving time of the solved instances_cp is "+str(Average_time)+ " seconds and the solved instances_cp are "+str(Solved_instances)+"/40" )
+    else:
+        print("no instance solved")
 
 def solve_SAT():
     print("[SAT solver]")
@@ -46,6 +51,6 @@ def solve_SAT():
 
 
 if __name__ == '__main__':
-    solve_cp()
-    #solve_SAT()
+    solve_cp("./CP/src/parsa/base_model.mzn")
+    solve_SAT()
 
