fb88ab442f591b493ac1d871923d9105370c4fad
Parsa Dahesh <dinoco.parsa23@gmail.com>
2022-09-08 00:32:12 +0200

    CP main updated

--- CP/src/main.py
+++ CP/src/main.py
@@ -1,49 +1,44 @@
-import datetime
-import minizinc as mz
+import os
+import subprocess
+from time import time
 
-def solve_cp(model_path):
-	tot_instances = 40
-	solved_instances = 0
-	avg_time = 0
-	gecode = mz.Solver.lookup("gecode")
 
-	for i in range(1, tot_instances + 1):
+def solve_instance(cores, model, in_file, out_dir):
+	# Run the chosen model
+	command = f'minizinc --solver Gecode -p {cores} -t 300000 {model} {in_file}' 
 
-		print(f"Instance {i}/{tot_instances}")
+	instance_name = in_file.split('/')[-1] if os.name == 'nt' else in_file.split('/')[-1]
+	instance_num = instance_name[4:len(instance_name) - 4]
+	out_file = os.path.join(out_dir, "out-"+instance_num+".txt")
+	with open(out_file, 'w') as f:
+		print(f'{out_file}:', end='\n', flush=True)
+		start_time = time()
+		subprocess.run(command.split())
+		elapsed_time = time() - start_time
+		print(f'{elapsed_time * 1000:.1f} ms')
+		if (elapsed_time * 1000) < 300000:
+			subprocess.run(command.split(), stdout=f)
+			f.write('{}'.format(elapsed_time))
 
-		VLSI_model = mz.Model(model_path)
-		VLSI_model.add_file("./CP/ins/ins-"+str(i)+".dzn")
-		instance = mz.Instance(gecode, VLSI_model)
-		
-		result = instance.solve(timeout = datetime.timedelta(seconds=300), # 5 min timeout
-								processes = 8,
-								optimisation_level = 1)
-		
-		print(result)
+def main():	
+	models = ["base_model_v1.mzn", 
+		"base_model_v2.mzn",
+		"base_model_v3.mzn",
+		"base_model_v4.mzn",
+		"base_model_v5.mzn",
+		"base_model_v6.mzn",
+		"base_model_v7.mzn",
+		"base_model_v8.mzn"]
 
-		if result.status == mz.result.Status.OPTIMAL_SOLUTION:
-			solved_instances+=1
-			
-			solving_time = ((result.statistics['time'].microseconds / (10 ** 6)) + result.statistics['time'].seconds)
-			avg_time += solving_time
-			print("Solved!")
-			print(f"Elapsed time: {str(round(solving_time, 3))}")
-			
-			with open('./CP/out/base/out-' + str(i) + '.txt', 'w') as writefile:
-				writefile.write(str(result))
-		else:
-			print("Timeout!")
-		
-		print()
+	cores = 8
+	ins_dir = "./CP/ins/"
+	out_dir = "./CP/out/base/"
+	model_dir = "./CP/src/models/"
 	
-	if solved_instances!=0:
-		avg_time = avg_time/solved_instances
-		print(f"Solved instances: {str(solved_instances)}/{tot_instances}")
-		print(f"Average solving time: {str(avg_time)}s")
-	else:
-		print("No instances solved :(")
-
+	for model in models:
+		for in_file in os.listdir(ins_dir):
+			solve_instance(cores, f"{model_dir}{model}", f"{ins_dir}{in_file}", out_dir)
 
 if __name__ == '__main__':
-	solve_cp("./CP/src/models/base_model_v1.mzn")
+	main()
 	
