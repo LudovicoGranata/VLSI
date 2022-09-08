7f9dd174afb14d3737e59a779a629d7d6529f986
Simone Persiani <iosonopersia@gmail.com>
2022-09-08 18:04:24 +0200

    Fix script read_solving_time

--- CP/src/read_solving_time.py
+++ CP/src/read_solving_time.py
@@ -6,14 +6,14 @@ solved = 0
 avg_time = 0.0
 
 for file in os.listdir(out_dir):
-	with open(file, 'r') as f:
-		last_line = f.readlines()[-1]
+	with open(os.path.join(out_dir, file), 'r') as f:
 		try:
+			last_line = float(f.readlines()[-1])
 			float(last_line)
-		except ValueError:
+		except:
 			continue
 		solved += 1
-		avg_time += float(last_line)
+		avg_time += last_line
 
 print(f"Solved {solved}/40 instances")
 print(f"Average solving time: {avg_time/solved}s")
